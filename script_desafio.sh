d5477d006f1b67bd3337b9b45b65afcd277a466e
root <root@JACKSON>
2023-02-14 23:55:24 -0300

    arquivo IaC

--- script_desafio.sh
+++ script_desafio.sh
@@ -7,7 +7,9 @@ mkdir /ven
 mkdir /sec
 
 echo "CRIAR GRUPOS"
-groupadd GRP_ADM,GRP_VEN,GRP_SEC
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
 
 
 echo "CRIAR USUÁRIOS"
@@ -17,7 +19,7 @@ useradd joao -c "Joao" -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
 
 useradd debora -c "Debora" -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
 useradd sebastiana -c "Sebastiana" -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
-useradd roberto -c "Roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd roberto -c "Roberto" -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
 
 useradd josefina -c "Josefina" -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
 useradd amanda -c "Amanda" -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
@@ -25,20 +27,25 @@ useradd rogerio -c "Rogerio" -m -s /bin/bash -p $(openssl passwd -crypt Senha123
 
 
 echo "APLICAR USUÁRIOS EM CADA GRUPO"
-usermod -G publico,adm carlos
-usermod -G publico,adm maria
-usermod -G publico,adm joao
+usermod -G GRP_ADM carlos
+usermod -G GRP_ADM maria
+usermod -G GRP_ADM joao
 
-usermod -G publico,ven debora
-usermod -G publico,ven sebastiana
-usermod -G publico,ven roberto
+usermod -G GRP_VEN debora
+usermod -G GRP_VEN sebastiana
+usermod -G GRP_VEN roberto
 
-usermod -G publico,sec josefina
-usermod -G publico,sec amanda
-usermod -G publico,sec rogerio
+usermod -G GRP_SEC josefina
+usermod -G GRP_SEC amanda
+usermod -G GRP_SEC rogerio
 
 
 echo "APLICAR AS PERMISSÕES"
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
 chmod 777 /publico/
 chmod 770 /adm/
 chmod 770 /ven/
